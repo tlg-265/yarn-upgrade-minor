570619aad97ce1fe0a3a7eb41d9fa0b20559204e
Angel Hernandez <angel@theleadgroup.com>
2020-02-10 15:07:25 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const outdated = require("outdated");
+
+function is_updatable_to(dep) {
+  var local_v = dep.local.split('.').map(x => parseInt(x));
+  var wanted_v = dep.wanted.split('.').map(x => parseInt(x));
+  var latest_v = dep.latest.split('.').map(x => parseInt(x));
+  // omitting some comparisons because assuming some facts
+  // comparing major versions (latest)
+  if (latest_v[0] === local_v[0]) {
+    // if different, then latest will be newer than local
+    if (dep.latest !== dep.local) {
+      return dep.latest;
+    }
+  }
+  // comparing major versions (wanted)
+  else if (wanted_v[0] === local_v[0]) {
+    // if different, then wanted will be newer than local
+    if (dep.wanted !== dep.local) {
+      return dep.wanted;
+    }
+  }
+  return false;
+}
+
+function update_dependencies(deps) {
+  console.log(`Amount of dependencies to update their minor version: ${deps.length}\n`);
+  for (var i = 0; i < deps.length; i++) {
+    var index = deps[i].index;
+    var name = deps[i].name;
+    var local = deps[i].local;
+    var wanted = deps[i].wanted;
+    var latest = deps[i].latest;
+    var target = deps[i].target;
+    console.log(`[${index}] name: ${name} | local: ${local} | wanted: ${wanted} | latest: ${latest} | target: ${target}`);
+  }
+  console.log();
+  console.log(`Command to run:\n`);
+  var command = `yarn upgrade`;
+  var deps_list = '';
+  for (var i = 0; i < deps.length; i++) {
+    var index = deps[i].index;
+    var name = deps[i].name;
+    var local = deps[i].local;
+    var wanted = deps[i].wanted;
+    var latest = deps[i].latest;
+    var target = deps[i].target;
+    deps_list += ` ${name}@^${target}`;
+  }
+  if (deps_list !== '') {
+    command += deps_list;
+    console.log(command);
+  }
+  else {
+    console.log('There is nothing to update right now.');
+  }
+}
+
+console.log(`Gathering information. Please, wait few seconds...\n`);
+
+outdated({
+  silent: true,
+  all: false,
+  ask: false,
+  prune: false,
+  update: false,
+  jsonUpdate: false,
+  verbose: 0,
+  bower: false,
+  jspm: false
+}).then(function(context) {
+  var result = [];
+  for (var i = 0, j = 1; i < context.dependencies.npm.length; i++) {
+    var dep = context.dependencies.npm[i];
+    if (dep['current'] === undefined) {
+      continue;
+    }
+    var target_ver = is_updatable_to(dep);
+    if (!target_ver) {
+      continue
+    }
+    var elem = {
+      index: j++,
+      name: dep.name,
+      local: dep.local,
+      wanted: dep.wanted,
+      latest: dep.latest,
+      target: target_ver
+    };
+    result.push(elem);
+  }
+  update_dependencies(result);
+});
